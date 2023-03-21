c83744644da2775f84bd186956e683078ab900e1
Yewolf <yyewolf@gmail.com>
2023-03-21 13:17:31 +0100

    feat(auth): added auth by email or tag

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -15,24 +15,6 @@ from app.schemas.responses import AccessTokenResponse
 
 router = APIRouter()
 
-@router.post("/access-token-card", response_model=AccessTokenResponse)
-async def login_access_token(
-    session: AsyncSession = Depends(deps.get_session),
-    form_data: OAuth2PasswordRequestForm = Depends(),
-):
-    """OAuth2 compatible token, get an access token for future requests using username and password"""
-
-    result = await session.execute(select(User).where(User.email == form_data.username))
-    user = result.scalars().first()
-
-    if user is None:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
-
-    if not security.verify_password(form_data.password, user.hashed_password):
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
-
-    return security.generate_access_token_response(str(user.id))
-
 @router.post("/access-token", response_model=AccessTokenResponse)
 async def login_access_token(
     session: AsyncSession = Depends(deps.get_session),
@@ -42,12 +24,16 @@ async def login_access_token(
 
     result = await session.execute(select(User).where(User.email == form_data.username))
     user = result.scalars().first()
+    
+    if user is None:
+        result = await session.execute(select(User).where(User.tag == form_data.username))
+        user = result.scalars().first()
 
     if user is None:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
+        raise HTTPException(status_code=400, detail="Incorrect email/tag or password")
 
     if not security.verify_password(form_data.password, user.hashed_password):
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
+        raise HTTPException(status_code=400, detail="Incorrect email/tag or password")
 
     return security.generate_access_token_response(str(user.id))
 
